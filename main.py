8cbb9f699808941f9f1a3f02b1726be925225127
Alejandro Diaz <alejandro.diaz@axians.es>
2022-08-10 09:36:49 +0200

    REFACTOR for readability

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import requests
 from properties import API_KEY
 import logging
 
-logging.basicConfig(filename="log.log", encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(filename="log.log", encoding='utf-8', level=logging.DEBUG, \
+                    format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 def todays_image():
     try:
